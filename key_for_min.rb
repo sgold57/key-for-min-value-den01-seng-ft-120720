d019763067ea8e1dd76e68a37df80987d150ac31
Sam Gold <sgold5757@gmail.com>
2020-10-15 18:10:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_value = 0
+  key_to_return = nil
   name_hash.each do |name, value|
     if min_value = 0 or value < min_value
       min_value = value
       key_to_return = name
+    end
   end
   key_to_return
 
